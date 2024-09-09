8dfaa25d702fde683968b773891f8d6e7ea11f2b
sonicffno1 <zff17243@gmail.com>
2024-09-09 11:00:21 +0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php 
 $servername = "localhost";
 $username = "root";
-$password= "";
-try{
-    $conn = new PDO("mysql:host=$severname;dbname=regis_db;charset=utf8", $username, $password);
+$password = "";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=regis_db;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected Successfully";
-} catch(PDOException $e){
-    echo "Connected failed : " . $e->getMessage();
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
